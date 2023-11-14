c76d393e052d328823718270941400189f33f02f
brunoalbin23 <bruno.albin@correo.ucu.edu.uy>
2023-11-14 17:18:45 -0300

    Nuevo proyecto xq hice cualquier cosa en el otro, por ahora solo cree las clases de las pantallas y el main

---
+++ obligatorioBD2/src/main/java/com/mycompany/obligatoriobd2/PantallaAgenda.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.obligatoriobd2;
+
+/**
+ *
+ * @author albin
+ */
+public class PantallaAgenda {
+    
+}
